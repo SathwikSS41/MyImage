1d8cb09932d7e2510d5a80862fc9ca3f43cd4934
Sathw <sathwik0411@gmail.com>
2023-12-29 13:52:38 +0530

    project completed

---
+++ app/src/main/java/com/example/myimage/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void btn(View view){
+        Intent x=new Intent(MainActivity.this, MainActivity2.class);
+        startActivity(x);
+    }
+}
